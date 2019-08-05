980f5e974345330f999b91cf7889992083e9f8a1
myname21-meet <myname21@meet.mit.edu>
2019-08-05 03:23:13 -0400

    flask

--- app.py
+++ app.py
@@ -9,8 +9,11 @@ app = Flask(__name__)
 
 # App routing code here
 @app.route('/')
-def home():
-    return render_template('home.html')
+def index():
+    return render_template('index.html')
+@app.route('/about')
+def about():
+	return render_template('about.html')
 
 # Running the Flask app
 if __name__ == "__main__":
